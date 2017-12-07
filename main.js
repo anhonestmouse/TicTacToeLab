7cc44a166533569bdf04b54ffa8131f13461c6d1
anhonestmouse <anhonestmouse@gmail.com>
2017-12-06 17:28:29 -0700

    game logic works.  need to fix restart button

--- main.js
+++ main.js
@@ -3,6 +3,23 @@ $(document).ready(function(){
 // let initialize = function(){
 	// clear board first, then:
 	let clickCounter = 0;
+	function checkForWin(){
+		console.log($('.X.leftColumn').length);
+		console.log($('.X.leftColumn'));
+		if(($('.X.leftColumn').length===3)||($('.X.middleColumn').length===3)||($('.X.rightColumn').length===3)||($('.X.topRow').length===3)||($('.X.middleRow').length===3)||($('.X.bottomRow').length===3)||($('.X.leftDiagonal').length===3)||($('.X.rightDiagonal').length===3)){
+			// checks if there are any 3 x's in a row
+			alert("X Wins!  WOOO!");
+			$('.box').addClass('clicked');
+		}else if(($('.O.leftColumn').length===3)||($('.O.middleColumn').length===3)||($('.O.rightColumn').length===3)||($('.O.topRow').length===3)||($('.O.middleRow').length===3)||($('.O.bottomRow').length===3)||($('.O.leftDiagonal').length===3)||($('.O.rightDiagonal').length===3)){
+		// checks if there are any 3 x's in a row
+		alert("O Wins!  WOOO!");
+		$('.box').addClass('clicked');
+		}else if(clickCounter ===9){
+			alert("It's a Tie!  Players Switch");
+		}
+	}
+
+
 	$('.box').click(function(){
 		if(!($(this).hasClass('clicked'))){
 			if(clickCounter%2===0){
@@ -20,9 +37,13 @@ $(document).ready(function(){
 				$(this).css('color','#079AFF');
 			}
 			clickCounter+=1;
-			// checkForWin();
+			checkForWin();
 		}else{
-			alert('This has already been selected.  Please choose an empty cell!');
+			if($(this).hasClass('X')||$('this').hasClass('O')){
+				alert('This has already been selected.  Please choose an empty cell!');
+			}else{
+				alert('The game is over.  Please push the restart button');
+			}
 		}
 	});
 });
